8415dc8467289f3d185037b8b72e504e35e7e27a
PR CI Handler <pr-ci-handler@auto>
2026-08-08 15:07:51 +0000

    fix: CI failure

--- gqlforge-tracker/src/collect/mod.rs
+++ gqlforge-tracker/src/collect/mod.rs
@@ -5,7 +5,6 @@ pub mod posthog;
 
 ///
 /// Defines the interface for an event collector.
-#[expect(clippy::double_must_use)]
 #[async_trait::async_trait]
 pub trait Collect: Send + Sync {
     async fn collect(&self, event: Event) -> super::Result<()>;
